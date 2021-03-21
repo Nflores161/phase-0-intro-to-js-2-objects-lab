ab9f62869c2e487bbbe9beb6ed16dc6ab565d19c
Nicholas Flores-Tober <nflores161@gmail.com>
2021-03-20 23:27:02 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+ const employee = {
+  name : 'Nick',
+  streetAddress : '108 Cooper St.'
+ }
+
+ function updateEmployeeWithKeyAndValue(employee, key, value) {
+   return Object.assign({}, employee, {[key]: value});
+ }
+
+ function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+   return Object.assign(employee, {[key]: value});
+ }
+
+ function deleteFromEmployeeByKey(employee, key) {
+  const newObj = Object.assign({}, employee)
+  delete newObj[key];
+  return newObj;
+ }
+
+ function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  const newObj = Object.assign(employee)
+  delete newObj[key];
+  return newObj;
+ }
